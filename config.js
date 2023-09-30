4a97cc53f412cbbb186589377e2018b5f7248847
ChidoriNC2 <122858863+dungnc2@users.noreply.github.com>
2023-09-30 22:19:47 +0700

    config db

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ const mysql = require("mysql");
 
 const connectionPool = mysql.createPool({
   connectionLimit: 10,
-  host: "127.0.0.1",
-  user: "ha",
-  password: "ha@123456",
-  database: "shop_shoe",
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "souq",
 });
 
 module.exports = connectionPool;
