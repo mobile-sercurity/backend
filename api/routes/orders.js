3e9fbadaee71b0387a0d9c74243317a51784e077
HA-1-N <hoanganh02062001@gmail.com>
2023-10-04 14:30:08 +0700

    feat(ordering): build api filter and update

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,127 +1,132 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 // import file
-const database = require("../../config")
+const database = require("../../config");
 
-const util = require('../../utils/mail');
-const util2 = require('../../utils/encrypt');
+const util = require("../../utils/mail");
+const util2 = require("../../utils/encrypt");
+const {
+  filterOrder,
+  updateOrder,
+} = require("../../controller/order-controller");
 
 // Order a product
 router.post("/add", (request, response) => {
-    var status = request.body.status
-    const name_on_card = request.body.name_on_card
-    var card_number = request.body.card_number
-    const expiration_date = request.body.expiration_date
-    const userId = request.body.userId
-    const productId = request.body.productId
-    var order_number;
+  var status = request.body.status;
+  const name_on_card = request.body.name_on_card;
+  var card_number = request.body.card_number;
+  const expiration_date = request.body.expiration_date;
+  const userId = request.body.userId;
+  const productId = request.body.productId;
+  var order_number;
 
-    card_number = util2.encrypt(card_number)
-     
-    const queryCategory = 'SELECT category FROM product WHERE id = ?'
-    database.query(queryCategory,productId, (error, result) => {
-        if(error) throw error;
+  card_number = util2.encrypt(card_number);
 
-        result = result[0]["category"]
-       
-        if(result == "mobile"){
-            order_number = '55' + util.getRandomInt(100000, 999999)
-        }else if(result == "laptop"){
-            order_number = '66' + util.getRandomInt(100000, 999999)
-        }else if(result == "baby"){
-            order_number = '77' + util.getRandomInt(100000, 999999)
-        }else if(result == "toy"){
-            order_number = '88' + util.getRandomInt(100000, 999999)
+  const queryCategory = "SELECT category FROM product WHERE id = ?";
+  database.query(queryCategory, productId, (error, result) => {
+    if (error) throw error;
+
+    result = result[0]["category"];
+
+    if (result == "mobile") {
+      order_number = "55" + util.getRandomInt(100000, 999999);
+    } else if (result == "laptop") {
+      order_number = "66" + util.getRandomInt(100000, 999999);
+    } else if (result == "baby") {
+      order_number = "77" + util.getRandomInt(100000, 999999);
+    } else if (result == "toy") {
+      order_number = "88" + util.getRandomInt(100000, 999999);
+    }
+
+    if (typeof status == "undefined" && status == null) {
+      status = "shipped";
+    }
+
+    const query =
+      "INSERT INTO Ordering(order_number, order_date ,status,name_on_card, card_number,expiration_date,user_id, product_id) VALUES(?,NOW(),?,?,?,?,?,?)";
+    const args = [
+      order_number,
+      status,
+      name_on_card,
+      card_number,
+      expiration_date,
+      userId,
+      productId,
+    ];
+
+    database.query(query, args, (error, result) => {
+      if (error) {
+        if (error.code === "ER_DUP_ENTRY") {
+          response.status(500).send("Deplicate Entry");
+        } else {
+          throw error;
         }
-
-        if(typeof status == 'undefined' && status == null){
-            status = "shipped";
-        }
-
-        const query = "INSERT INTO Ordering(order_number, order_date ,status,name_on_card, card_number,expiration_date,user_id, product_id) VALUES(?,NOW(),?,?,?,?,?,?)"
-        const args = [order_number,status, name_on_card, card_number, expiration_date, userId, productId]
-
-        database.query(query, args, (error, result) => {
-            if (error) {
-                if (error.code === 'ER_DUP_ENTRY') {
-                    response.status(500).send("Deplicate Entry")
-                } else {
-                    throw error;
-                }
-            } else {
-                response.status(200).send("You ordered a product")
-            }
-        });
-
-        
-    })
+      } else {
+        response.status(200).send("You ordered a product");
+      }
+    });
+  });
 });
-
 
 router.get("/", (request, response) => {
-    const productId = request.body.id
+  const productId = request.body.id;
 
-    var order_number;
+  var order_number;
 
-    const queryCategory = 'SELECT category FROM product WHERE id = ?'
-    database.query(queryCategory,productId, (error, result) => {
-        if(error) throw error;
+  const queryCategory = "SELECT category FROM product WHERE id = ?";
+  database.query(queryCategory, productId, (error, result) => {
+    if (error) throw error;
 
-        result = result[0]["category"]
+    result = result[0]["category"];
 
-        console.log(result)
-       
-        if(result === "mobile"){
-            console.log('hello')
-            order_number = 55 + getRandomInt(100000, 999999)
-        }else if(result == "laptop"){
-            order_number = 66 + getRandomInt(100000, 999999)
-        }else if(result == "baby"){
-            order_number = 77 + getRandomInt(100000, 999999)
-        }else if(result == "toy"){
-            order_number = 88 + getRandomInt(100000, 999999)
-        }
+    console.log(result);
 
-        response.status(200).json({
-            "category" : result
-        })
-    });
-
-    function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min)) + min; 
+    if (result === "mobile") {
+      console.log("hello");
+      order_number = 55 + getRandomInt(100000, 999999);
+    } else if (result == "laptop") {
+      order_number = 66 + getRandomInt(100000, 999999);
+    } else if (result == "baby") {
+      order_number = 77 + getRandomInt(100000, 999999);
+    } else if (result == "toy") {
+      order_number = 88 + getRandomInt(100000, 999999);
     }
-});
 
+    response.status(200).json({
+      category: result,
+    });
+  });
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+});
 
 // Get Orders
 router.get("/get", (request, response) => {
-    var userId = request.query.userId;
-    var page = request.query.page;
-    var page_size = request.query.page_size;
+  var userId = request.query.userId;
+  var page = request.query.page;
+  var page_size = request.query.page_size;
 
-    if(page == null || page < 1){
-        page = 1;
-    }
- 
-    if(page_size == null){
-        page_size = 20;
-    }
+  if (page == null || page < 1) {
+    page = 1;
+  }
 
-    // OFFSET starts from zero
-    const offset = page - 1;
-    // OFFSET * LIMIT
-    page = offset * page_size;
+  if (page_size == null) {
+    page_size = 20;
+  }
 
-    const args = [
-        userId,
-        parseInt(page_size),
-        parseInt(page)
-    ];
+  // OFFSET starts from zero
+  const offset = page - 1;
+  // OFFSET * LIMIT
+  page = offset * page_size;
 
-    const query = `SELECT DISTINCT Ordering.order_number,
+  const args = [userId, parseInt(page_size), parseInt(page)];
+
+  const query = `SELECT DISTINCT Ordering.order_number,
                           DATE_FORMAT(Ordering.order_date, '%d/%m/%Y') As order_date, 
                           Ordering.status,Product.product_name,
                           Product.price,
@@ -131,17 +136,20 @@ router.get("/get", (request, response) => {
                           FROM Ordering JOIN Product JOIN User JOIN Shipping 
                           ON Ordering.product_id = product.id AND Ordering.user_id = user.id AND Ordering.product_id = Shipping.product_id
                           WHERE Ordering.user_id = ? 
-                          LIMIT ? OFFSET ?`
+                          LIMIT ? OFFSET ?`;
 
-    database.query(query, args, (error, orders) => {
-        if(error) throw error;
-        response.status(200).json({
-            "page": offset + 1,
-            "error" : false,
-            "orders" : orders
-        })
-       
-    })
+  database.query(query, args, (error, orders) => {
+    if (error) throw error;
+    response.status(200).json({
+      page: offset + 1,
+      error: false,
+      orders: orders,
+    });
+  });
 });
 
-module.exports = router
+// filter order
+router.post("/filter", filterOrder);
+router.post("/update", updateOrder);
+
+module.exports = router;
---
+++ controller/order-controller.js
@@ -0,0 +1,98 @@
+const database = require("../config");
+
+// filter ordering
+const filterOrder = async (req, res) => {
+  const { page = 1, limit = 10 } = req.query;
+  const orderNumber = req.body.orderNumber;
+  const status = req.body.status;
+
+  let query = " SELECT COUNT(*) AS total FROM ordering ";
+  let params = [];
+
+  if (orderNumber) {
+    query += " WHERE order_number LIKE ? ";
+    params.push(`%${orderNumber}%`);
+  }
+
+  if (status) {
+    if (orderNumber) {
+      query += " AND ";
+    } else {
+      query += " WHERE ";
+    }
+    query += " status LIKE ?";
+    params.push(`%${status}%`);
+  }
+
+  database.query(query, params, (error, results) => {
+    if (error) {
+      console.error("Lỗi truy vấn CSDL:", error);
+      res.status(500).json({ error: "Đã xảy ra lỗi khi truy vấn CSDL" });
+      return;
+    }
+
+    const total = results[0].total;
+    const totalPage = Math.ceil(total / limit);
+
+    const offset = (page - 1) * limit;
+    const limitParam = parseInt(limit);
+
+    let filterQuery = "Select * from ordering";
+    let filterParams = [];
+
+    if (orderNumber) {
+      filterQuery += " WHERE order_number LIKE ?";
+      filterParams.push(`%${orderNumber}%`);
+    }
+
+    if (status) {
+      if (orderNumber) {
+        filterQuery += " AND ";
+      } else {
+        filterQuery += " WHERE ";
+      }
+      filterQuery += "status LIKE ?";
+      filterParams.push(`%${status}%`);
+    }
+
+    filterQuery += ` LIMIT ?, ?; `;
+    filterParams.push(offset, limitParam);
+
+    database.query(filterQuery, filterParams, (error, data) => {
+      if (error) {
+        console.error("Lỗi truy vấn cơ sở dữ liệu:", error);
+        res
+          .status(500)
+          .json({ error: "Đã xảy ra lỗi khi truy vấn cơ sở dữ liệu" });
+        return;
+      }
+
+      res.json({
+        total,
+        totalPage: totalPage,
+        page: parseInt(page),
+        limit: limitParam,
+        data,
+      });
+    });
+  });
+};
+
+// update ordering
+const updateOrder = async (req, res) => {
+  const id = req.body.id;
+  const status = req.body.status;
+
+  let query = "UPDATE ordering SET status = ? WHERE ordering_id = ?";
+  let params = [status, id];
+
+  database.query(query, params, (error, result) => {
+    if (error) throw error;
+
+    res.status(200).json({
+      message: "Order updated successfully",
+    });
+  });
+};
+
+module.exports = { filterOrder, updateOrder };
